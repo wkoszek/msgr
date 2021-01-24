c3489d45fc56a837a1a452c3d5a454f73437d164
Adam Koszek <adam@koszek.com>
2021-01-24 15:21:13 -0800

    Add more stuff.

--- context.go
+++ context.go
@@ -1,16 +1,16 @@
 package main
 
-
 // Context holds a current application state
 type Context struct {
-	Config		*Config
+	Config *Config
 
-	ArgFrom		*string
-	ArgTo		*string
-	ArgSubject	*string
-	ArgWhere	*string
-	ArgProfileName	*string
-	ArgCode		*bool
-	ArgHTML		*bool
-	ArgDry		*bool
+	ArgFrom        *string
+	ArgTo          *string
+	ArgSubject     *string
+	ArgWhere       *string
+	ArgProfileName *string
+	ArgCode        *bool
+	ArgHTML        *bool
+	ArgDry         *bool
+	ArgVerbose     *bool
 }
